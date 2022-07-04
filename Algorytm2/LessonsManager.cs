bf4b7372e1a98daf2c4a045862f511912f189244
Ilona Lapka <ilona2000123@wp.pl>
2022-07-04 19:29:05 +0200

    Init

---
+++ Algorytm2/LessonsManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorytm2
+{
+    public class LessonsManager
+    {
+        public List<Lesson> list { get; set; } = new List<Lesson>();
+
+        public void CreateLesson(int start, int end)
+        {
+            if(start < end)
+            {
+                Lesson lesson = new Lesson() { Start = start, End = end };
+                list.Add(lesson);
+            }
+            else
+            {
+                Console.WriteLine("Nieprawidłowy format lekcji.");
+            }
+        }
+
+        public void SortingList()
+        {
+            list = list.OrderBy(a => a.Start).ThenBy(b => b.End).ToList();
+        }
+
+        public void CountRooms()
+        {
+            int count = 1;
+            int currentEnd = list[0].End;
+
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i].Start < currentEnd)
+                {
+                    count++;
+                }
+                else
+                {
+                    currentEnd = list.Where(a => a.End > currentEnd).ToList().OrderBy(a => a.End)
+                        .First().End;
+                }
+            }
+
+            Console.WriteLine("Najmniejsza potrzebna liczba sal, tak aby mogły się odbyć " +
+                "zajęcia to {0}", count);
+        }
+    }
+}
---
+++ Algorytm2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorytm2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LessonsManager lessonsManager = new LessonsManager();
+            lessonsManager.CreateLesson(0, 75);
+            lessonsManager.CreateLesson(0, 35);
+            lessonsManager.CreateLesson(40, 90);
+            lessonsManager.CreateLesson(20, 70);
+
+            lessonsManager.SortingList();
+
+            lessonsManager.CountRooms();
+        }
+    }
+}
